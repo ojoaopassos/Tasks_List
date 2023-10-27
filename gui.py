cbb1115fe7254409a3f09c526afcb49d39daf01c
jvspa <jvspassos2@gmail.com>
2023-10-27 16:32:51 -0300

    Implemented Edit button

--- gui.py
+++ gui.py
@@ -5,10 +5,18 @@ label = PySimpleGUI.Text("Type in a task:")
 input_box = PySimpleGUI.InputText(tooltip="Enter Task", key="task")
 add_button = PySimpleGUI.Button("Add")
 
+tasks_box = PySimpleGUI.Listbox(values=functions.get_tasks(), key="selected_task",
+								enable_events=True, size=[45, 10])
+
+edit_button = PySimpleGUI.Button("Edit")
+
 exit_button = PySimpleGUI.Button("Exit")
 
 window = PySimpleGUI.Window("My Tasks App",
-							layout=[[label], [input_box, add_button], [exit_button]],
+							layout=[[label],
+									[input_box, add_button],
+									[tasks_box, edit_button],
+									[exit_button]],
 							font=("Helvetica", 20))
 
 while True:
@@ -25,6 +33,27 @@ while True:
 			tasks.append(task_to_append)
 
 			functions.write_tasks(tasks)
+
+			window["selected_task"].update(values=tasks)
+
+		case "Edit":
+			tasks = functions.get_tasks()
+
+			task_to_edit = value["selected_task"][0]
+
+			new_task = value["task"] + "\n"
+			new_task = new_task.capitalize()
+
+			index = tasks.index(task_to_edit)
+
+			tasks[index] = new_task
+
+			functions.write_tasks(tasks)
+
+			window["selected_task"].update(tasks)
+
+		case "selected_task":
+			window["task"].update(value=value["selected_task"][0])
 
 		case "Exit":
 			break
